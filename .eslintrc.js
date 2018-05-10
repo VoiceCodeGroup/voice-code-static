d3a98d9c0497b52af4365ff068251089eec15e57
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-05-10 16:26:10 +1200

    Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: 'react-tools'
-}
+  extends: 'react-tools',
+  rules: {
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    semi: [2, 'always']
+  }
+};
--- src/containers/About.js
+++ src/containers/About.js
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React from 'react';
 //
 
 export default () => (
@@ -7,4 +6,4 @@ export default () => (
     <h1>This is what we're all about.</h1>
     <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
   </div>
-)
+);
