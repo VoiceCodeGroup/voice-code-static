d7c050ca6994d4c1441a8d8c6edc6dde7193a353
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-05-10 16:39:01 +1200

    Test fetching data

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,9 +1,25 @@
-import React from 'react';
-//
+import React, { Component } from 'react';
 
-export default () => (
-  <div>
-    <h1>This is what we're all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
-  </div>
-);
+export default class Fetch extends Component {
+  state = { data: "This is what we're all about" };
+
+  constructor(props) {
+    super(props);
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(json => this.hasData('Hi'));
+  }
+  hasData = data => {
+    this.setState({ data });
+  };
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div>
+        <h1>{this.state.data}</h1>
+        <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
+      </div>
+    );
+  }
+}
